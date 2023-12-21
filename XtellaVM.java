ef91220638c43c12937ce88062e5c782507d8fab
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-12-21 16:37:02 +0330

    added: several more bytecode methods

--- XtellaVM.java
+++ XtellaVM.java
@@ -239,6 +239,178 @@ public class XtellaVM {
     frame_pointer++;
   }
 
+  private void executeShiftLeft() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for SHIFT_LEFT");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      int result = (int) operand2 << (int) operand1;
+      stack.push(result);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for SHIFT_LEFT");
+    }
+
+    frame_pointer++;
+  }
+
+  private void executeShiftRight() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for SHIFT_RIGHT");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      int result = (int) operand2 >> (int) operand1;
+      stack.push(result);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for SHIFT_RIGHT");
+    }
+
+    frame_pointer++;
+  }
+
+  private void executeEqual() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for EQUAL");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    boolean result;
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      result = ((int) operand1) == ((int) operand2);
+    } else if (operand1 instanceof Float && operand2 instanceof Float) {
+      result = Float.compare((float) operand1, (float) operand2) == 0;
+    } else if (operand1 instanceof String && operand2 instanceof String) {
+      result = operand1.equals(operand2);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for EQUAL");
+    }
+
+    stack.push(result);
+    frame_pointer++;
+  }
+
+  private void executeNotEqual() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for NOT_EQUAL");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    boolean result;
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      result = ((int) operand1) != ((int) operand2);
+    } else if (operand1 instanceof Float && operand2 instanceof Float) {
+      result = Float.compare((float) operand1, (float) operand2) != 0;
+    } else if (operand1 instanceof String && operand2 instanceof String) {
+      result = !operand1.equals(operand2);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for NOT_EQUAL");
+    }
+
+    stack.push(result);
+    frame_pointer++;
+  }
+
+  private void executeLessThan() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for LESS_THAN");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    boolean result;
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      result = ((int) operand2) < ((int) operand1);
+    } else if (operand1 instanceof Float && operand2 instanceof Float) {
+      result = ((float) operand2) < ((float) operand1);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for LESS_THAN");
+    }
+
+    stack.push(result);
+    frame_pointer++;
+  }
+
+  private void executeLessThanOrEqual() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for LESS_THAN_OR_EQUAL");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    boolean result;
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      result = ((int) operand2) <= ((int) operand1);
+    } else if (operand1 instanceof Float && operand2 instanceof Float) {
+      result = ((float) operand2) <= ((float) operand1);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for LESS_THAN_OR_EQUAL");
+    }
+
+    stack.push(result);
+    frame_pointer++;
+  }
+
+  private void executeGreaterThan() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for GREATER_THAN");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    boolean result;
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      result = ((int) operand2) > ((int) operand1);
+    } else if (operand1 instanceof Float && operand2 instanceof Float) {
+      result = ((float) operand2) > ((float) operand1);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for GREATER_THAN");
+    }
+
+    stack.push(result);
+    frame_pointer++;
+  }
+
+  private void executeGreaterThanOrEqual() {
+    if (stack.size() < 2) {
+      throw new IllegalStateException("Not enough operands on the stack for GREATER_THAN_OR_EQUAL");
+    }
+
+    Object operand1 = stack.pop();
+    Object operand2 = stack.pop();
+
+    boolean result;
+
+    if (operand1 instanceof Integer && operand2 instanceof Integer) {
+      result = ((int) operand2) >= ((int) operand1);
+    } else if (operand1 instanceof Float && operand2 instanceof Float) {
+      result = ((float) operand2) >= ((float) operand1);
+    } else {
+      throw new IllegalArgumentException("Invalid operand types for GREATER_THAN_OR_EQUAL");
+    }
+
+    stack.push(result);
+    frame_pointer++;
+  }
+
   private void executeReturn() {
     if (frame_pointer == 0) {
       throw new IllegalStateException("Cannot RETURN from the main frame");
@@ -246,6 +418,43 @@ public class XtellaVM {
 
     frame_pointer--;
     instruction_pointer = 0;
+  }
+
+  private void executeJump() {
+    // Get the target instruction index from the stack
+    int targetIndex = (int) stack.get(instruction_pointer);
+    // Set the instruction pointer to the target index
+    instruction_pointer = targetIndex;
+  }
+
+  private void executeJumpIfTrue() {
+    // Pop the condition from the stack
+    boolean condition = (boolean) stack.pop();
+    // Get the target instruction index from the stack
+    int targetIndex = (int) stack.get(instruction_pointer);
+
+    if (condition) {
+      // If the condition is true, set the instruction pointer to the target index
+      instruction_pointer = targetIndex;
+    } else {
+      // Otherwise, move to the next instruction
+      instruction_pointer++;
+    }
+  }
+
+  private void executeJumpIfFalse() {
+    // Pop the condition from the stack
+    boolean condition = (boolean) stack.pop();
+    // Get the target instruction index from the stack
+    int targetIndex = (int) stack.get(instruction_pointer);
+
+    if (!condition) {
+      // If the condition is false, set the instruction pointer to the target index
+      instruction_pointer = targetIndex;
+    } else {
+      // Otherwise, move to the next instruction
+      instruction_pointer++;
+    }
   }
 
   public void executeNextInstruction() {
